ea23d328b6b20edb8446c91d1abf156d57475092
Vinicius Zago <zvinicius@hotmail.com>
2025-06-04 10:46:36 -0300

    Ajuste na lógica de cálculo de troco e tempo no parquímetro

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 class Parquimetro {
     constructor() {
-        this.valorPor30Min = 1.00;
+        this.preco30min = 1.00;
+        this.preco60min = 1.75;
+        this.preco120min = 3.00;
     }
 
     calcularTempo() {
@@ -14,30 +16,32 @@ class Parquimetro {
         trocoElemento.textContent = "";
         mensagemElemento.textContent = "";
 
-        if (isNaN(valor) || valor < 1.00) {
-            mensagemElemento.textContent = "⚠️ Valor insuficiente. Insira no mínimo R$1,00.";
+        if (isNaN(valor) || valor < this.preco30min) {
+            mensagemElemento.textContent = "Valor insuficiente. Insira no mínimo R$1,00.";
             return;
         }
 
         let tempo = 0;
         let troco = 0;
 
-        if (valor >= 3) {
-            tempo = 120; // 2 horas máximo
-            troco = valor - 3;
-        } else if (valor >= 2) {
+        if (valor >= this.preco120min) {
+            tempo = 120;
+            troco = (valor - this.preco120min).toFixed(2);
+        } else if (valor >= this.preco60min) {
             tempo = 60;
-            troco = valor - 2;
+            troco = (valor - this.preco60min).toFixed(2);
         } else {
             tempo = 30;
-            troco = valor - 1;
+            troco = (valor - this.preco30min).toFixed(2);
         }
 
         tempoElemento.textContent = `Tempo de Estacionamento: ${tempo} minutos.`;
-        trocoElemento.textContent = `Troco: R$ ${troco.toFixed(2)}.`;
+        trocoElemento.textContent = `Troco: R$ ${troco}.`;
         mensagemElemento.textContent = "Operação realizada com sucesso!";
     }
 }
 
 // Instancia do objeto
 const parquimetro = new Parquimetro();
+
+
